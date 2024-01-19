7ddecbb75cb367fcc27924c5f4e73abd8fe97fcc
carey-andrew <carey_andrew@hotmail.com>
2024-01-19 13:42:36 +0000

    user inputs

--- python101.py
+++ python101.py
@@ -1,5 +1,5 @@
 #print a new string then reverse it
-msg='Welcome to Python 101: Strings'
+"""msg='Welcome to Python 101: Strings'
 msg1=msg[18]+' '+msg[:8]+msg[25:29]+msg[7:11]+msg[13]+msg[12]+msg[2]+msg[1]+msg[-5]
 print(msg1.title()) #capitalize first letter of each word
 print(msg1[::-1].title())#reverse string
@@ -17,4 +17,5 @@ color='Green'
 msg3='['+name+'] loves the color '+color.lower()+'!'
 msg4=f'{name.capitalize()} loves the color {color.lower()}!'
 print(msg3)
-print(msg4)
+print(msg4)"""
+
