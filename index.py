e1aacce030e582961457bf0cf832516ef3778e9e
carey-andrew <carey_andrew@hotmail.com>
2024-01-19 10:57:33 +0000

    first 4 tickets

---
+++ index.py
@@ -0,0 +1,25 @@
+"""print("Welcome to python")
+print("Create hammer")
+print("Create nails")
+
+print("Use hammer and nails")
+
+failed_subjects="2.56"
+name="John"
+print("Dear Mrs Badger")
+
+print("Your son " + name + " is failing " + str(failed_subjects) + " subjects.")
+name="Eric"
+print(name + " is doing well in geography.")
+
+print(type('hello'))
+print(type(1))
+print(type(1.63))
+print(type(True))
+
+print('Variables & Datatypes - Exercise 1')
+item_name='widget'
+price=2.5
+inventory=100
+is_in_inventory=True
+print(item_name, price, inventory)"""
