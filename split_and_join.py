0eecb98e2895617139d763a097910455de050b6e
carey-andrew <carey_andrew@hotmail.com>
2024-01-25 14:43:42 +0000

    split and join

---
+++ split_and_join.py
@@ -0,0 +1,13 @@
+msg = 'Welcome to python'
+csv = 'Eric,John,Michael,Terry,Graham'
+friends_list = ['Eric','John','Michael','Terry','Graham']
+print(msg.split())
+print(msg.split(' '))
+print(csv.split(','))#split on comma
+
+print('-'.join(friends_list))#join list with hyphen
+print(''.join(friends_list))#join list with nothing inbetween
+print(' '.join(friends_list))#join list with space inbetween
+
+print(' '.join(msg.split()))
+print(msg.replace(' ',''))
