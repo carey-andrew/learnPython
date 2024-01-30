f9e3fb5161de31cbd5c87b567146a79039aa7d5d
carey-andrew <carey_andrew@hotmail.com>
2024-01-30 10:26:26 +0000

    tuples

---
+++ tuple.py
@@ -0,0 +1,9 @@
+#tuples - faster lists you cant change
+friends = ['John','Michael','Ollie','Eric']
+friends_tuple = ('John','Michael','Ollie','Eric')#tuples use () instead of []
+
+print(friends[2:4])
+print(friends_tuple[2:4])
+
+#cannot use remove or append or pop on tuples
+
