35825571ea5f430c996c1b25c00bf05f3260f2c5
carey-andrew <carey_andrew@hotmail.com>
2024-01-31 14:47:36 +0000

    python calculator

---
+++ if_else_exercise.py
@@ -0,0 +1,25 @@
+#calculator exercise
+#create a calc that handles +,-,*,/ and outputs the resultbased on the mode / operator used 
+# use 3 seperate inputs
+# bonus to include a celsius to fahrenheit converter
+# formula is (c * 9/5) + 32 = f
+
+first_number = float(input('Enter first number: '))
+operator = input('Enter operator: ')
+second_number = float(input('Enter second number: '))
+
+if operator == '+':
+    print(first_number + second_number)
+elif operator == '-':
+    print(first_number - second_number)
+elif operator == '*':
+    print(first_number * second_number)
+elif operator == '/':
+    print(first_number / second_number)
+else:
+    print('Invalid operator')
+
+celsius = float(input('Enter temperature in celsius: ')) 
+fahrenheit = (celsius * 9/5) + 32
+print(fahrenheit)
+
