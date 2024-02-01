308d65dffa8d63acd157dfea579605857330ecf7
carey-andrew <carey_andrew@hotmail.com>
2024-02-01 11:20:24 +0000

    while loop

---
+++ while_loops.py
@@ -0,0 +1,13 @@
+#whiile loops
+#1. what do i want to repeat?
+#2. what do i want to change each time?
+#3. how long should we repeat?
+
+i=0
+#while i < 5 :
+    #print("Hello World")
+    #i+=1
+#print same thing five times
+while i < 5 :
+    i+=1    
+    print(f'{i}.'+ '*'*i + 'Loops are awesome!' + '*'*i)
