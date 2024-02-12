0f38488d0fc01e556eee416b653e68b630631cfc
carey-andrew <carey_andrew@hotmail.com>
2024-02-12 17:06:30 +0000

    python modules

---
+++ modules.py
@@ -0,0 +1,22 @@
+# modules
+
+import platform
+#import platform, string, os # import multiple modules
+
+import platform as pl # alias
+print(dir(platform))
+
+print(platform.python_version())
+
+
+
+# imports module and all its functions and gives information about the module
+
+# datetime
+
+# random
+# math
+# os
+# sys
+# json
+# requests
